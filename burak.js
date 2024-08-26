891af20a0d15494fcbfb839f104979f504fb6fe7
brkmrc35 <brcmrc1453@gmail.com>
2024-08-26 21:28:19 +0300

    yuvarla

---
+++ burak.js
@@ -0,0 +1,8 @@
+function BURAK_yuvarla(){
+    let sayiniz = prompt("yuvarlanacak sayıyı giriniz");
+    let yuvarla = Math.round(sayiniz);
+
+    alert(`söylediğiniz sayının yuvarlanmış hali ${yuvarla}`);
+}
+
+BURAK_yuvarla()
