f8a0f5ffefca466d76ce51d0f21b5c63cd41b8cb
brkmrc35 <brcmrc1453@gmail.com>
2024-09-02 21:50:34 +0300

    forrr

--- burak.forrr.js
+++ burak.forrr.js
@@ -1,7 +1,7 @@
 function carpaniYazdir(sayi) {
     console.log(`${sayi} sayısının çarpanları:`);
 
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= sayi; i++) {
         if (sayi % i === 0) {
             console.log(i);
         }
