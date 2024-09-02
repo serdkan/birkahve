281f91d70b0f77e474058dcfba0d11edd80cccd1
serdkan <serdar.kandiran@outlook.com>
2024-09-02 21:56:35 +0300

    importlar 2

--- serdar.js
+++ serdar.js
@@ -1,6 +1,7 @@
 import { sevdiklerim } from "./burak-test2.js";
-import { days } from "./omer2-test.js";
+import { days as gunler } from "./omer2-test.js";
 import { teams } from "./efe-test2.js";
 import { sayi } from "./fatih-test.js";
+import { days as dayss } from "./mazhar-test.js";
 
-console.log(sayi);
+console.log(gunler);
