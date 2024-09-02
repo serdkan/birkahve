993a0710ea1bc6e25b9e13c244f5fc2f78cc49d9
serdkan <serdar.kandiran@outlook.com>
2024-09-02 21:51:08 +0300

    improtlar

--- serdar.js
+++ serdar.js
@@ -1,3 +1,6 @@
-import { days } from "./serdar-test2.js";
+import { sevdiklerim } from "./burak-test2.js";
+import { days } from "./omer2-test.js";
+import { teams } from "./efe-test2.js";
+import { sayi } from "./fatih-test.js";
 
-console.log(days);
+console.log(sayi);
