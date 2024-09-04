e30d6cc9bf747698e8d246bdfb304d31a32f2cff
brkmrc35 <brcmrc1453@gmail.com>
2024-09-04 23:07:11 +0300

    function odevi

---
+++ burak.function.js
@@ -0,0 +1,19 @@
+function takimkurulusu(takimAdi)
+{
+   if(takimAdi == "GÖZTEPE")
+   {
+      console.log(1925);
+   }
+
+   if(takimAdi == "Galatasaray")
+   {
+      console.log(1905);
+   }
+
+   if(takimAdi == "Beşiktaş")
+   {
+    console.log(1903); 
+   }
+}
+
+takimkurulusu("GÖZTEPE")
