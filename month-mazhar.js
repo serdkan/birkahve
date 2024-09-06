89ca02842536ccb19aac5b6e80f545301b38b37c
mazharemir1453 <mazharemir23@gmail.com>
2024-09-06 21:10:59 +0300

    mazhar3

---
+++ month-mazhar.js
@@ -0,0 +1,12 @@
+function month(ttrName) {
+if(trName=="ocak"){
+    console.log("January");
+}
+if(trName=="şuba"){
+    console.log("February");
+}
+if(trName=="mart"){
+    console.log("march");
+}
+}
+export { mounth };
