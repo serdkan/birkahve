f4d5a097e40850d62402b7ee1bfa2543b53fd80d
serdkan <serdar.kandiran@outlook.com>
2024-09-06 21:15:21 +0300

    month fonksiyon yazıldı

---
+++ month-serdar.js
@@ -0,0 +1,13 @@
+function month(trName) {
+  if (trName == "ocak") {
+    console.log("January");
+  }
+  if (trName == "şubat") {
+    console.log("February");
+  }
+  if (trName == "mart") {
+    console.log("march");
+  }
+}
+
+export { month };
