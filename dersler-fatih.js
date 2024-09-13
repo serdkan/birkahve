f78a6b341a758acc951bb5befda2be9fdfd287eb
LimonSTAR5555 <biguktibi@gmail.com>
2024-09-13 22:21:44 +0300

    yenilendi

--- dersler-fatih.js
+++ dersler-fatih.js
@@ -1,7 +1,2 @@
-function yanlis(sayi1,sayi2,sayi3){
-    let topla = sayi1 + sayi2 + sayi3;
-    console.log(topla);
-}
-yanlis(11,22,33);
-
-export{ yanlis };
+let haftalikdersler = [ "fen","matematik"];
+let günlükdersler = ["ingilizce","din"];
